b5a5c2fba848d40307d2a0fe8bb49025b53ab1d1
liangqiangc <liangqiangc@yonyou.com>
2018-05-03 15:40:26 +0800

    create style
    create style ,and add ImageData jSON file

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,30 @@ require('styles/App.scss');
 
 import React from 'react';
 
+//import Image JSON data
+var ImageJSonArr = require('../data/imageData.json');
+
+
+ImageJSonArr = (function(ImageArr){
+  ImageArr.map(function(item,index,array){
+    //imageUrl props Bind
+    item.imageURL = require('../images/'+item.fileName);
+    array[index] = item;
+  });
+  return ImageArr;
+})(ImageJSonArr);
+
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div>hello world</div>
-      </div>
+      <section className="stage">
+          <section className="image-sec">
+          </section>
+          <nav className="controller-nav">
+          </nav>
+      </section>
     );
   }
 }
